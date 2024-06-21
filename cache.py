f7d83081f9dbd120c65600ffbd6757c593d549d3
shnmrt <sahinmurat3@gmail.com>
2024-06-21 14:54:26 +0200

    feat: changes in table fields

--- cache.py
+++ cache.py
@@ -6,7 +6,7 @@ class Database:
         class to maintain and interact with db
     """
     def __init__(self, catalog_name) -> None:
-        self.db_connection, self.db_cursor = self._get_connection(catalog_name)
+        self.connection = self._get_connection(catalog_name)
         
 
     def create(self) -> None:
@@ -20,24 +20,27 @@ class Database:
 
     def delete(self) -> None:
         pass
-    
-    def _get_connection(cls, catalog_name) -> tuple:
+    def _get_connection(self, catalog_name) -> tuple:
         # check is there is a file called `db`
         plugin_dir = os.path.dirname(__file__)
         
         if not os.path.isdir(f'{plugin_dir}/db'):
             os.mkdir(f'{plugin_dir}/db')
+            print("directory called db is created")
         
         # check if exist folder called `catalog_name`
         if not os.path.isfile(f'{plugin_dir}/db/{catalog_name}.db'):
+            print(f"creating the db for {catalog_name}!")
             connection = sqlite3.connect(f"{plugin_dir}/db/{catalog_name}.db")
             cursor = connection.cursor()
-            cursor.execute("CREATE TABLE collection(uuid, id, title, description)")
-            cursor.execute("CREATE TABLE item(uuid, id, collecion_id, datetime)")
-            cursor.execute("CREATE TABLE asset(uuid, id, item_id, href, title)")
-            return (connection, cursor)
+            cursor.execute("CREATE TABLE catalog(id, title, description)")
+            cursor.execute("CREATE TABLE collection(id, title, description, sld, qml)")
+            cursor.execute("CREATE TABLE item(id, collection_id)")
+            cursor.execute("CREATE TABLE asset(id, item_id, href)")
+            print("tables are populated")
         else:
             connection = sqlite3.connect(f"{plugin_dir}/db/{catalog_name}.db")
-            cursor = connection.cursor()
-            return (connection, cursor)
-            
+            print("connection is established!")
+         
+        return connection
+        
