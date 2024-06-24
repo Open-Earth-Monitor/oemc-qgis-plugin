b51024e12bc49cb58092e1db3910c81444563657
shnmrt <sahinmurat3@gmail.com>
2024-06-24 14:50:36 +0200

    feat: implementation of the db insertions

--- cache.py
+++ cache.py
@@ -7,11 +7,8 @@ class Database:
     """
     def __init__(self, catalog_name) -> None:
         self.connection = self._get_connection(catalog_name)
-        
-
-    def create(self) -> None:
-        pass
-    
+        self.cursor = self.connection.cursor()
+            
     def read(self) -> None:
         pass
 
@@ -20,6 +17,7 @@ class Database:
 
     def delete(self) -> None:
         pass
+
     def _get_connection(self, catalog_name) -> tuple:
         # check is there is a file called `db`
         plugin_dir = os.path.dirname(__file__)
@@ -27,20 +25,47 @@ class Database:
         if not os.path.isdir(f'{plugin_dir}/db'):
             os.mkdir(f'{plugin_dir}/db')
             print("directory called db is created")
+        db_path = f"{plugin_dir}/db/{catalog_name}.db"
         
         # check if exist folder called `catalog_name`
-        if not os.path.isfile(f'{plugin_dir}/db/{catalog_name}.db'):
-            print(f"creating the db for {catalog_name}!")
-            connection = sqlite3.connect(f"{plugin_dir}/db/{catalog_name}.db")
-            cursor = connection.cursor()
-            cursor.execute("CREATE TABLE catalog(id, title, description)")
-            cursor.execute("CREATE TABLE collection(id, title, description, sld, qml)")
-            cursor.execute("CREATE TABLE item(id, collection_id)")
-            cursor.execute("CREATE TABLE asset(id, item_id, href)")
-            print("tables are populated")
+        if not os.path.isfile(db_path):
+            connection = sqlite3.connect(db_path)
+            self._create_db(connection=connection)
+            
         else:
-            connection = sqlite3.connect(f"{plugin_dir}/db/{catalog_name}.db")
-            print("connection is established!")
-         
+            connection = sqlite3.connect(db_path)
+
         return connection
-        
+
+
+    def _create_db(self, connection):
+        self.cursor.execute("CREATE TABLE catalog(id, title, description)")
+        self.cursor.execute("CREATE TABLE collection(id, title, description, sld, qml)")
+        self.cursor.execute("CREATE TABLE item(id, collection_id)")
+        self.cursor.execute("CREATE TABLE asset(id, item_id, href)")        
+    
+    # single event selection case.
+    def insert_catalog(self, id, title, description):
+        self.cursor.execute(f"INSERT INTO catalog VALUES ({id},{title},{description})")
+        self.connection.commit()
+
+    # Single entry event. The user only allowed to select a collection from the UI
+    # when the selection is performed on the UI following function needed to be called. 
+    def insert_collection(self, id, title, description, sld, qml):
+        self.cursor.execute(f"INSET INTO collection VALUES ({id},{title},{description},{sld},{qml})")
+        self.connection.commit()
+
+    # The user allowed to select multiple items which needed to be submit into db 
+    def insert_items(self, ids, collection_id):
+        # multiple id for item
+        # single id for collection_id
+        data = [(id, collection_id) for id in ids]
+        self.cursor.executemany("INSERT INTO item VALUES(?,?)", data)
+        self.connection.commit()
+
+    def insert_assets(self, ids, item_id, hrefs):
+        # multiple id for assets
+        # single id for the items that assets belong to
+        data = [(id, item_id ,href) for id, href in zip(ids, hrefs)]
+        self.cursor.executemany("INSERT INTO asset VALUES(?,?,?)", data)
+        self.connection.commit()
